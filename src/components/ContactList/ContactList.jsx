bb8a460bb23e12dd84befee0ec7fdb41482cc08e
Sergei Moskalyov <sv.moskalyov@gmail.com>
2023-02-08 15:39:25 +0200

    Create ContactList.jsx

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,11 @@
+export const ContactList = ({ contacts }) => {
+  return (
+    <ul>
+      {contacts.map(({ id, name, number }) => (
+        <li key={id}>
+          {name}: {number}
+        </li>
+      ))}
+    </ul>
+  );
+};
