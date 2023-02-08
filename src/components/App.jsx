c6876d37fc4d28b08b088ac242cb9ac60004fb0b
Sergei Moskalyov <sv.moskalyov@gmail.com>
2023-02-08 10:44:34 +0200

    adds state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,11 @@ import { Component } from 'react';
 import { Box } from './Box';
 
 export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
   render() {
     return (
       <Box width="420px" mx="auto" p={4}>
