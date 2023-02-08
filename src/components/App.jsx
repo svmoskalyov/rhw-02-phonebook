6bdcf1e7f942ef67899b101052c84ba94ff75efa
Sergei Moskalyov <sv.moskalyov@gmail.com>
2023-02-08 10:35:50 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,12 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { Box } from './Box';
+
+export class App extends Component {
+  render() {
+    return (
+      <Box width="420px" mx="auto" p={4}>
+        React homework template
+      </Box>
+    );
+  }
+}
