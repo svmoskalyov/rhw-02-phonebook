06aa5fff608ca0460033cac5376cd34112f64cd1
Sergei Moskalyov <sv.moskalyov@gmail.com>
2023-02-08 15:39:28 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,9 @@
 import { Component } from 'react';
+import { nanoid } from 'nanoid';
 import { Box } from './Box';
+import { Section } from './Section/Section';
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
 
 export class App extends Component {
   state = {
@@ -7,10 +11,28 @@ export class App extends Component {
     name: '',
   };
 
+  formSubmitHandler = ({ name, number }) => {
+    const item = {
+      id: nanoid(),
+      name,
+      number,
+    };
+
+    this.setState(({ contacts }) => ({
+      contacts: [item, ...contacts],
+    }));
+  };
+
   render() {
     return (
-      <Box width="420px" mx="auto" p={4}>
-        React homework template
+      <Box width="500px" mx="auto" p={4}>
+        <Section title="Phonebook">
+          <ContactForm onSubmit={this.formSubmitHandler} />
+        </Section>
+
+        <Section title="Contacts">
+          <ContactList contacts={this.state.contacts} />
+        </Section>
       </Box>
     );
   }
