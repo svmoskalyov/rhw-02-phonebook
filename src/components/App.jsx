830e81c778b0ee5ea879589475b90df3677125f3
Sergei Moskalyov <sv.moskalyov@gmail.com>
2023-02-08 16:24:25 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,11 +4,17 @@ import { Box } from './Box';
 import { Section } from './Section/Section';
 import { ContactForm } from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
 
 export class App extends Component {
   state = {
-    contacts: [],
-    name: '',
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
   };
 
   formSubmitHandler = ({ name, number }) => {
@@ -31,6 +37,7 @@ export class App extends Component {
         </Section>
 
         <Section title="Contacts">
+          <Filter />
           <ContactList contacts={this.state.contacts} />
         </Section>
       </Box>
