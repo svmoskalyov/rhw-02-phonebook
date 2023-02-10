82a160badc799ae7eb730509dbf07d5ab97e3c48
Sergei Moskalyov <sv.moskalyov@gmail.com>
2023-02-10 19:50:37 +0200

    Update ContactForm.styled.jsx

--- src/components/ContactForm/ContactForm.styled.jsx
+++ src/components/ContactForm/ContactForm.styled.jsx
@@ -7,6 +7,10 @@ export const Forma = styled(Form)`
   padding: ${p => p.theme.space[4]}px;
   margin-bottom: ${p => p.theme.space[4]}px;
   border: ${p => p.theme.borders.normal};
+
+  button:last-child {
+    margin: auto;
+  }
 `;
 
 export const Label = styled.label`
@@ -22,24 +26,4 @@ export const Input = styled(Field)`
   padding: 0 ${p => p.theme.space[3]}px;
   width: 90%;
   font-size: ${p => p.theme.fontSizes.m};
-`;
-
-export const Button = styled.button`
-  display: inline-flex;
-  justify-content: center;
-  align-items: center;
-  margin: auto;
-  padding-top: ${p => p.theme.space[3]}px;
-  padding-bottom: ${p => p.theme.space[3]}px;
-  padding-left: ${p => p.theme.space[4]}px;
-  padding-right: ${p => p.theme.space[4]}px;
-  border: ${p => p.theme.borders.normal};
-  border-radius: ${p => p.theme.radii.normal};
-  background-color: ${p => p.theme.colors.primary};
-  color: ${p => p.theme.colors.white};
-  cursor: pointer;
-
-  :hover {
-    background-color: ${p => p.theme.colors.secondary};
-  }
 `;
