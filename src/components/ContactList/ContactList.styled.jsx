16ce964f8e955c86c2cbf4efa53ef0c1ae5e1f61
Sergei Moskalyov <sv.moskalyov@gmail.com>
2023-02-10 15:41:25 +0200

    Update ContactList.styled.jsx

--- src/components/ContactList/ContactList.styled.jsx
+++ src/components/ContactList/ContactList.styled.jsx
@@ -1,14 +1,36 @@
 import styled from 'styled-components';
 
 export const List = styled.ul`
-  padding-left: ${p => p.theme.space[4]}px;
+  padding-left: ${p => p.theme.space[5]}px;
+  width: 60%;
 `;
 
 export const Item = styled.li`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
   margin-bottom: ${p => p.theme.space[2]}px;
   font-family: ${p => p.theme.fonts.body};
   font-size: ${p => p.theme.fontSizes.m};
   font-weight: ${p => p.theme.fontWeights.normal};
   line-height: ${p => p.theme.lineHeights.body};
-  list-style: inside;
+`;
+
+export const Button = styled.button`
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  padding-top: ${p => p.theme.space[2]}px;
+  padding-bottom: ${p => p.theme.space[2]}px;
+  padding-left: ${p => p.theme.space[4]}px;
+  padding-right: ${p => p.theme.space[4]}px;
+  border: ${p => p.theme.borders.normal};
+  border-radius: ${p => p.theme.radii.normal};
+  background-color: ${p => p.theme.colors.primary};
+  color: ${p => p.theme.colors.white};
+  cursor: pointer;
+
+  :hover {
+    background-color: ${p => p.theme.colors.secondary};
+  }
 `;
