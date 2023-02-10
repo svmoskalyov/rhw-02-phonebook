6a1494c0ce61dbf017da30207e87930bfb24ddee
Sergei Moskalyov <sv.moskalyov@gmail.com>
2023-02-10 19:50:43 +0200

    Update ContactList.styled.jsx

--- src/components/ContactList/ContactList.styled.jsx
+++ src/components/ContactList/ContactList.styled.jsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const List = styled.ul`
   padding-left: ${p => p.theme.space[5]}px;
-  width: 60%;
+  width: 70%;
 `;
 
 export const Item = styled.li`
@@ -14,23 +14,4 @@ export const Item = styled.li`
   font-size: ${p => p.theme.fontSizes.m};
   font-weight: ${p => p.theme.fontWeights.normal};
   line-height: ${p => p.theme.lineHeights.body};
-`;
-
-export const Button = styled.button`
-  display: inline-flex;
-  align-items: center;
-  justify-content: center;
-  padding-top: ${p => p.theme.space[2]}px;
-  padding-bottom: ${p => p.theme.space[2]}px;
-  padding-left: ${p => p.theme.space[4]}px;
-  padding-right: ${p => p.theme.space[4]}px;
-  border: ${p => p.theme.borders.normal};
-  border-radius: ${p => p.theme.radii.normal};
-  background-color: ${p => p.theme.colors.primary};
-  color: ${p => p.theme.colors.white};
-  cursor: pointer;
-
-  :hover {
-    background-color: ${p => p.theme.colors.secondary};
-  }
 `;
