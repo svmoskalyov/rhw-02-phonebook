ef4d7010f8374f25350f6c9d1c36ec3a391f9776
Sergei Moskalyov <sv.moskalyov@gmail.com>
2023-02-11 17:39:27 +0200

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import { Label, Input } from './Filter.styled';
 
 export const Filter = ({ value, onChange }) => {
@@ -16,4 +17,9 @@ export const Filter = ({ value, onChange }) => {
       />
     </Label>
   );
+};
+
+Filter.propTypes = {
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
 };
